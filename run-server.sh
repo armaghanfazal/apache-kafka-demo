a768db724b7fade69ce94b72fcd7b71810c7dba5
Armaghan Fazal <armghan255@gmail.com>
2023-01-13 11:09:39 +0500

    :tada: initial commit

---
+++ run-server.sh
@@ -0,0 +1,5 @@
+# Run zookeeper cluster manager
+/usr/local/bin/zookeeper-server-start /usr/local/etc/zookeeper/zoo.cfg
+
+# Run apache kafka server
+/usr/local/bin/kafka-server-start /usr/local/etc/kafka/server.properties
